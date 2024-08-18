59a02d380a438bbfbb8c493addc6ecbaf97c5ecd
PrajwolKhadka <prazolkhadka67@gmail.com>
2024-08-18 16:43:32 +0545

    Innit

---
+++ ClinicManagementApp/src/main/java/com/carrental/clinicmanagementapp/ClinicManagementApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.carrental.clinicmanagementapp;
+
+/**
+ *
+ * @author Acer
+ */
+public class ClinicManagementApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
